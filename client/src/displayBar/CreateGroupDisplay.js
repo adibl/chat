7278fda8d18b7e646c56b3407f6c763e1f2f858d
adi bleyer <bleyer23@gmail.com>
2020-09-30 12:23:33 +0300

    split create group display

--- client/src/displayBar/CreateGroupDisplay.js
+++ client/src/displayBar/CreateGroupDisplay.js
@@ -1,15 +1,9 @@
 import React, {useContext, useRef} from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
 import Button from "@material-ui/core/Button";
 import useUsersList from "../apiCalls/useUsersList";
 import TextField from "@material-ui/core/TextField";
 import config from "../apiCalls/config";
 import UserContext from "../usernameContex";
-import Typography from "@material-ui/core/Typography";
 import {IconButton} from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Card from "@material-ui/core/Card";
@@ -18,25 +12,13 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import UserMultiSelect from "./usersDisplay/usersMultiSelection";
 
 export default function CreateGroupDisplay(props) {
     const username = useContext(UserContext);
     const groupNameRef = useRef(null);
     const [usersData, refresh] = useUsersList();
     const [checked, setChecked] = React.useState([]);
-
-    const handleToggle = (user) => () => {
-        const currentIndex = checked.indexOf(user);
-        const newChecked = [...checked];
-
-        if (currentIndex === -1) {
-            newChecked.push(user);
-        } else {
-            newChecked.splice(currentIndex, 1);
-        }
-
-        setChecked(newChecked);
-    };
 
     function createGroup() {
         fetch(`${config.url}/conversations`, {
@@ -77,23 +59,7 @@ export default function CreateGroupDisplay(props) {
                     type="string"
                     inputRef={groupNameRef}
                 />
-                <List dense>
-                    {usersData.map((user, index) => {
-                        return <ListItem key={index}>
-                            <Typography variant="h5" component="h5">
-                                <ListItemText primary={user} disableTypography={true}/>
-                            </Typography>
-                            <ListItemSecondaryAction>
-                                <Checkbox
-                                    edge="end"
-                                    onChange={handleToggle(user)}
-                                    checked={checked.indexOf(user) !== -1}
-                                />
-                            </ListItemSecondaryAction>
-                        </ListItem>
-
-                    })}
-                </List>
+                <UserMultiSelect users={usersData} setChecked={setChecked} checked={checked}/>
             </CardContent>
             <CardActions>
                 <Button onClick={createGroup} size={"medium"}>
---
+++ client/src/displayBar/usersDisplay/usersMultiSelection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from "@material-ui/core/Typography";
+
+export default function UserMultiSelect(props) {
+
+    const handleToggle = (user) => () => {
+        const currentIndex = props.checked.indexOf(user);
+        const newChecked = [...props.checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(user);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        props.setChecked(newChecked);
+    };
+
+    return (
+        <List dense>
+            {props.users.map((user, index) => {
+                return <ListItem key={index}>
+                    <Typography variant="h5" component="h5">
+                        <ListItemText primary={user} disableTypography={true}/>
+                    </Typography>
+                    <ListItemSecondaryAction>
+                        <Checkbox
+                            edge="end"
+                            onChange={handleToggle(user)}
+                            checked={props.checked.indexOf(user) !== -1}
+                        />
+                    </ListItemSecondaryAction>
+                </ListItem>
+            })}
+        </List>
+    );
+}
